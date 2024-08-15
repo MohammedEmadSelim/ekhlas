45c93a307227f2b014b34545fa781c1a3c5777fc
mohamed.emad <mohammedemad557@yahoo.com>
2024-08-15 14:29:44 +0300

    splash screen

---
+++ lib/controlers/main_cubit/main_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'main_state.dart';
+
+class MainCubit extends Cubit<MainState> {
+  MainCubit() : super(MainInitial());
+  addFromExcelToFireBase(){}
+}
--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatefulWidget {
@@ -10,9 +11,51 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body:  Center(
-        child: Text('Main Page '),
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 80.0,
+        backgroundColor: Color(0xfffdfcdc),
+        actions: [
+          InkWell(
+            onTap: () {
+              print('pressed');
+              FirebaseFirestore.instance
+                  .collection('products')
+                  .add({"name": "ahmed"}).then((value) {
+                    print('successfully added');
+                  },);
+            },
+            child: Container(
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Color(0xfffdfcdc),
+                borderRadius: BorderRadius.all(Radius.circular(12)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.shade400.withOpacity(0.5),
+                    blurRadius: 6,
+                    offset: Offset(2, 2),
+                    spreadRadius: 1,
+                  ),
+                  BoxShadow(
+                    color: Colors.white,
+                    blurRadius: 6,
+                    offset: Offset(-1, -1),
+                    spreadRadius: 1,
+                  ),
+                ],
+              ),
+              child: Text('Uplaod Prices'),
+            ),
+          ),
+          const SizedBox(
+            width: 10,
+          )
+        ],
+      ),
+      backgroundColor: Color(0xfffdfcdc),
+      body: Center(
+        child: Text('Main Page'),
       ),
     );
   }
